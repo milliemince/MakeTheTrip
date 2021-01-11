597ddd591933aaef6013a4596ffcddb3e97435c3
milliemince <milliemince4@gmail.com>
2021-01-11 12:34:34 -0600

    Xcode project initialization

---
+++ MakeTheTrip/MakeTheTrip/MakeTheTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeTheTripApp.swift
+//  MakeTheTrip
+//
+//  Created by Camille Mince on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MakeTheTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
